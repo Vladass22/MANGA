2d0565200ae91611980ac49e6355b705de07ed4a
Vladass22 <107876852+Vladass22@users.noreply.github.com>
2022-07-28 11:31:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 //Popup form
 	document.getElementById('intro_btn').onclick = function() {
 		document.getElementById('main').classList.add('pop-open');
+		var $elem = document.querySelector('#bl3-ttl-1');
+		var text = $elem.textContent;
+		document.getElementById('find').value = text
 	}
 	document.getElementById('intro_btn2').onclick = function() {
 		document.getElementById('main').classList.add('pop-open');
+		var $elem = document.querySelector('#bl3-ttl-2');
+		var text = $elem.textContent;
+		document.getElementById('find').value = text
 	}
 	document.getElementById('intro_btn3').onclick = function() {
 		document.getElementById('main').classList.add('pop-open');
+		var $elem = document.querySelector('#bl3-ttl-3');
+		var text = $elem.textContent;
+		document.getElementById('find').value = text
 	}
 	document.getElementById('intro_btn4').onclick = function() {
 		document.getElementById('main').classList.add('pop-open');
+		var $elem = document.querySelector('#bl3-ttl-4');
+		var text = $elem.textContent;
+		document.getElementById('find').value = text
 	}
 	document.getElementById('pop_close').onclick = function() {
 		document.getElementById('main').classList.remove('pop-open');
